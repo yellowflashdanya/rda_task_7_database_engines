2e237351e9b8d9836837caf27f353e531d4676d7
Danylo Melnychuk <danyakid2005@gmail.com>
2025-05-07 00:31:44 +0300

    Solution

--- task.sql
+++ task.sql
@@ -4,7 +4,7 @@ USE ShopDB;
 -- Create a table to store countries 
 CREATE TABLE Countries (
     ID INT AUTO_INCREMENT,
-    Name VARCHAR(50),
+    Name VARCHAR(50) NOT NULL,
     PRIMARY KEY (ID)
 ) ENGINE=InnoDB;
 
@@ -20,9 +20,9 @@ CREATE TABLE GeoIP (
 -- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
 CREATE TABLE ProductDescription (
     ID INT AUTO_INCREMENT,
-    CountryID INT,
-    ProductID INT,
-    Description VARCHAR(100),
+    CountryID INT NOT NULL,
+    ProductID INT NOT NULL,
+    Description VARCHAR(100) NOT NULL,
     PRIMARY KEY (ID),
     FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE CASCADE
 ) ENGINE=InnoDB;
@@ -30,14 +30,14 @@ CREATE TABLE ProductDescription (
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
 CREATE TABLE Logs (
     ID INT AUTO_INCREMENT,
-    Time DATETIME,
-    LogRecord VARCHAR(50),
+    Time DATETIME NOT NULL,
+    LogRecord VARCHAR(50) NOT NULL,
     PRIMARY KEY (ID)
 ) ENGINE=BLACKHOLE;
 
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
 CREATE TABLE ProductReporting (
-    Date DATE,
-    ProductName VARCHAR(100),
-    Orders INT
-) ENGINE=CSV;
+    Date DATE NOT NULL,
+    ProductName VARCHAR(100) NOT NULL,
+    Orders INT NOT NULL
+) ENGINE=InnoDB;
